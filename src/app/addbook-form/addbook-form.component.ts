d2e57f85c2b1e3a421ec51324ad10fb2ea036423
Lucas <lucasferguson16@gmail.com>
2020-04-18 12:44:17 -0500

    css

--- src/app/addbook-form/addbook-form.component.ts
+++ src/app/addbook-form/addbook-form.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-addbook-form',
-  templateUrl: './addbook-form.component.html',
-  styleUrls: ['./addbook-form.component.css']
+	selector: 'addbook-form',
+	templateUrl: './addbook-form.component.html',
+	styleUrls: ['./addbook-form.component.css'],
 })
 export class AddbookFormComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
