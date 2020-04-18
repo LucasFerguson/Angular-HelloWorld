cf26f463fa7b6134d39fd7abcbf20a431e13f3d7
Lucas <lucasferguson16@gmail.com>
2020-04-18 13:00:10 -0500

    add navigation-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { DatabaseService } from './database.service';
 import { AddbookFormComponent } from './addbook-form/addbook-form.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
-	declarations: [AppComponent, BooksComponent, AddbookFormComponent],
+	declarations: [AppComponent, BooksComponent, AddbookFormComponent, NavigationBarComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [DatabaseService],
 	bootstrap: [AppComponent],
